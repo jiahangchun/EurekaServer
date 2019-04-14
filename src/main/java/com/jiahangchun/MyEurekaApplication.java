2bc73c6b46e5a68f8968382295262c371e05760b
jiahangchun <jia123456>
2019-04-14 11:52:38 +0800

    打包出问题的原因：没有建立main包

--- src/main/java/com/jiahangchun/MyEurekaApplication.java
+++ src/main/java/com/jiahangchun/MyEurekaApplication.java
@@ -1,15 +1,15 @@
 package com.jiahangchun;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class MyEurekaApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(MyEurekaApplication.class).web(true).run(args);
+        SpringApplication.run(MyEurekaApplication.class, args);
     }
 }
