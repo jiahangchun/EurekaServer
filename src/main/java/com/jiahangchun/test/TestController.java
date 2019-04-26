ff5535e309ac252f9830bfdaabcecc6498e853c6
jiahangchun <jia123456>
2019-04-26 23:12:32 +0800

    swagger 测试 & jdk版本问题

--- src/main/java/com/jiahangchun/test/TestController.java
+++ src/main/java/com/jiahangchun/test/TestController.java
@@ -1,11 +1,13 @@
 package com.jiahangchun.test;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value="测试接口Controller")
 public class TestController {
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String home() {
         return "Hello world";
     }
